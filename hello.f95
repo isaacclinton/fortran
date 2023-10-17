c5da0cbf6da354370b275166f7f6d2a7b736ca37
Isaac Clinton <apidaisaac@gmail.com>
2023-10-17 14:44:20 +0300

    different datatypes

--- hello.f95
+++ hello.f95
@@ -1,11 +1,26 @@
 program fortrantut
     implicit none
-    character*20 :: name
-    character (len = 20) :: f_name, l_name
-    print *, "What's your name ?"
+    
+    ! real - contains decimal
+    ! parameter - is a constant
+    real,parameter :: PI = 3.1415
 
-    read *, f_name, l_name
+    ! defining multiple variables
+    real :: r_num1 = 0.0, r_num2 = 1.1
 
-    print *, "Hello '", trim(f_name), "' Last name=", trim(l_name)
+    ! double. take note of d+0, it's needed
+    double precision :: dbl_num = 1.1d+0
 
+    ! integer
+    integer :: i_num1 = 0, i_num2 = 0
+
+    ! booleans
+    logical :: can_vote = .false.
+
+    print *,PI
+    print *, r_num1, r_num2
+    print *, dbl_num
+    print *, i_num1
+    print *, can_vote
+    
 end program fortrantut
